f0ede802fe7bdffb9323d70d300826519ba78f0f
Suraj Salgar <suraj935911@gmail.com>
2026-04-04 12:05:10 +0530

    Implemented in-memory user storage using List

--- src/main/java/com/suraj/usermanagement/controller/MyController.java
+++ src/main/java/com/suraj/usermanagement/controller/MyController.java
@@ -4,30 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class MyController {
+	
+	private List<User> userList = new ArrayList<>(); 
 
     @GetMapping("/test")
     public String test() {
         return "Working";
     }
     
-    @GetMapping("/user")
-    public User getUser() {
-    	
-    	User user = new User();
-    	
-    	user.setId(1);
-    	user.setName("Suraj");
-    	user.setEmail("suraj@gmail.com");
-    	
-    	return user;
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+  
+    	return userList;
     	
     }
     
     @PostMapping("/addUser")
     public User addUser(@RequestBody User user) {
+    	userList.add(user);
     	return user;
     	
     }
